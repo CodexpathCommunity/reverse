3a55a7ebe1b8ca1a139f8fc80c45c4d83900e7e0
CodexpathCommunity <codexpath3@gmail.com>
2022-04-17 07:31:04 +0100

    added reducers

---
+++ frontend/src/AppProvider.js
@@ -0,0 +1,13 @@
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import store from "./store";
+
+const AppProvider = ({ children }) => {
+  return (
+    <Provider store={store}>
+      <Router>{children}</Router>
+    </Provider>
+  );
+};
+
+export default AppProvider;
