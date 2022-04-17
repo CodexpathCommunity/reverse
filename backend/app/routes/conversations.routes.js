c9c9216f6c2ff0b684c4da6d6f2a5ab7df11549a
CodexpathCommunity <codexpath3@gmail.com>
2022-04-17 19:22:51 +0100

    added conversation services

--- backend/app/routes/conversations.routes.js
+++ backend/app/routes/conversations.routes.js
@@ -3,8 +3,21 @@ const router = express.Router();
 
 const {
   createConversation,
+  updateConversation,
+  getConversationById,
+  getUserConversations,
+  matchConversations,
 } = require("../controllers/conversations.controller");
 
+// create a conversation
 router.post("/", createConversation);
+// update conversaion
+router.put("/update/:id", updateConversation);
+//get conversation by id
+router.get("/read/:id", getConversationById);
+// get users conversation
+router.get("/user/:id", getUserConversations);
+// find conversation between two people
+router.get("/match/:firstUserId/:secondUserId", matchConversations);
 
 module.exports = router;
--- backend/app/services/conversations.service.js
+++ backend/app/services/conversations.service.js
@@ -24,6 +24,95 @@ const createConversationService = async (req) => {
   }
 };
 
+const updateConversationService = async (req) => {
+  try {
+    const conversation = await Conversation.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    return {
+      data: conversation,
+      error: false,
+      message: "success",
+      statusCode: 200,
+    };
+  } catch (error) {
+    return {
+      data: {},
+      error: true,
+      message: "Sorry an error occurred",
+      statusCode: 500,
+    };
+  }
+};
+const getConversationByIdService = async (req) => {
+  try {
+    const conversation = await Conversation.findById(req.params.id);
+    return {
+      data: conversation,
+      error: false,
+      message: "success",
+      statusCode: 200,
+    };
+  } catch (error) {
+    return {
+      data: {},
+      error: true,
+      message: "Sorry an error occurred",
+      statusCode: 500,
+    };
+  }
+};
+
+const getUserConversationService = async (req) => {
+  try {
+    const conversation = await Conversation.find({
+      members: { $in: [req.params.id] },
+    });
+    return {
+      data: conversation,
+      error: false,
+      message: "success",
+      statusCode: 200,
+    };
+  } catch (error) {
+    return {
+      data: {},
+      error: true,
+      message: "Sorry an error occurred",
+      statusCode: 500,
+    };
+  }
+};
+
+const matchConversationsService = async (req) => {
+  try {
+    const conversation = await Conversation.findOne({
+      members: { $all: [req.params.firstUserId, req.params.secondUserId] },
+    });
+    return {
+      data: conversation,
+      error: false,
+      message: "success",
+      statusCode: 200,
+    };
+  } catch (error) {
+    return {
+      data: {},
+      error: true,
+      message: "Sorry an error occurred",
+      statusCode: 500,
+    };
+  }
+};
+
 module.exports = {
   createConversationService,
+  updateConversationService,
+  getConversationByIdService,
+  getUserConversationService,
+  matchConversationsService,
 };
