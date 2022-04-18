53d739b36a8aff13ee85e83974305d3fa9519743
CodexpathCommunity <codexpath3@gmail.com>
2022-04-18 06:04:19 +0100

    creating message routes

--- backend/app/controllers/conversations.controller.js
+++ backend/app/controllers/conversations.controller.js
@@ -1,6 +1,10 @@
 const sse = require("../../sse");
 const {
   createConversationService,
+  updateConversationService,
+  getConversationByIdService,
+  getUserConversationService,
+  matchConversationsService,
 } = require("../services/conversations.service");
 //conversation route
 exports.createConversation = async (req, res) => {
@@ -14,4 +18,32 @@ exports.createConversation = async (req, res) => {
       console.log(error);
     }
   }
+};
+
+exports.updateConversation = async (req, res) => {
+  const result = await updateConversationService(req);
+  res.status(result.statusCode).json(result.data);
+  if (!result.error) {
+    //   emit post event
+    try {
+      sse.send(result.data, `conversation_${result.data.lastReciever}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+exports.getConversationById = async (req, res) => {
+  const result = await getConversationByIdService(req);
+  res.status(result.statusCode).json(result.data);
+};
+
+exports.getUserConversations = async (req, res) => {
+  const result = await getUserConversationService(req);
+  res.status(result.statusCode).json(result.data);
+};
+
+exports.matchConversations = async (req, res) => {
+  const result = await matchConversationsService(req);
+  res.status(result.statusCode).json(result.data);
 };
